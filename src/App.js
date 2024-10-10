fcbeaf46a168056839b3b0e6b2b419d776a8ed9f
taiwosugar <oguntoyinborichman@gmail.com>
2024-10-10 17:03:57 +0100

    hggh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './home/Home';
 import About from './details/About';
--- src/details/ScrollToTop .jsx
+++ src/details/ScrollToTop .jsx
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
     const { pathname } = useLocation();
   
     useEffect(() => {
-      window.scrollTo(0, 2); // Scroll to the top of the page when the route changes
+      window.scrollTo(0, 0); // Scroll to the top of the page when the route changes
     }, [pathname]);
   
     return null;
