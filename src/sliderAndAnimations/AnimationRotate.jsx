e422eb040dc2ac0c480d5ae47d1fb3958f3ad16b
taiwosugar <oguntoyinborichman@gmail.com>
2024-10-22 03:16:12 +0100

    gg

--- src/sliderAndAnimations/AnimationRotate.jsx
+++ src/sliderAndAnimations/AnimationRotate.jsx
@@ -7,12 +7,12 @@ const images = [
   { id: 2, src: "/image/web2-image.png", alt: "Image 2",},
   { id: 3, src: "/image/web3-image.png", alt: "Image 3" },
   { id: 4, src: "/image/web5-image.png", alt: "Image 4" },
-  { id: 5, src: "/image/web6-image.png", alt: "Image 5" },
+  { id: 5, src: "/image/web1-image.png", alt: "Image 5" },
   { id: 6, src: "/image/web7-image.png", alt: "Image 6" },
   { id: 7, src: "/image/web8-image.png", alt: "Image 7" },
   { id: 8, src: "/image/web4-image.png", alt: "Image 8" },
   { id: 9, src: "/image/web9.png", alt: "Image 9" },
-  { id: 10, src: "/image/exeter-image.png", alt: "Image 10" },
+  { id: 10, src: "/image/web20.png", alt: "Image 10" },
 ];
 
 const AnimationRotate = () => {
@@ -34,28 +34,3 @@ const AnimationRotate = () => {
 }
 
 export default AnimationRotate;
-
-// import React from 'react';
-// import './AnimationRotate.css';
-
-// const AnimationRotate = () => {
-//   return (
-//     <div className="banner">
-//       <div className="slider" style={{ "--quantity": 10 }}>
-//         <div className="item" style={{ "--position": 1 }}><img src="/image/image1.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 2 }}><img src="/image/image2.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 3 }}><img src="/image/image3.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 4 }}><img src="/image/image4.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 5 }}><img src="/image/image5.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 6 }}><img src="/image/image6.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 7 }}><img src="/image/image7.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 8 }}><img src="/image/image8.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 9 }}><img src="/image/image9.png" alt=""/></div>
-//         <div className="item" style={{ "--position": 10 }}><img src="/image/exeter-image.png" alt=""/></div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AnimationRotate;
-
